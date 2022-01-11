0a1236d8534bbb062bc2623a2dceade1296d4c34
v-zhangyiming1 <v-zhangyiming1@360.cn>
2022-01-11 11:09:59 +0800

    放弃直接读取文件的实现方式

--- controller/controller.go
+++ controller/controller.go
@@ -1,25 +1,23 @@
 package controller
 
 import (
-	"bufio"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/zhangyiming748/slackersCalendar/model"
 	"github.com/zhangyiming748/slackersCalendar/util/log"
-	"io"
-
 	"net/http"
-	"os"
-	"os/exec"
-
 )
 
 func Happy(ctx *gin.Context) {
-	line:=model.HappyTimer()
-	app1:=model.HappyDay()
-	model.Gift()
-	model.AnniversaryDay()
-	model.HappyFinnal()
+	line := model.HappyTimer()
+	app1 := model.HappyDay()
+	app2 := model.Gift()
+	app3 := model.AnniversaryDay()
+	app4 := model.HappyFinnal()
+	line = append(line, app1...)
+	line = append(line, app2...)
+	line = append(line, app3...)
+	line = append(line, app4...)
 	content := HappyJson{
 		Title:    "Happy",
 		Contents: line,
@@ -28,12 +26,12 @@ func Happy(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, content)
 }
 func Sad(ctx *gin.Context) {
-	model.SadTimer()
-	model.SadDay()
-	info:=readLine("./info.txt")
+	line := model.SadTimer()
+	app1 := model.SadDay()
+	line = append(line, app1...)
 	content := SadJson{
 		Title:    "Happy",
-		Contents: info,
+		Contents: line,
 		Ip:       getRequestIP(ctx),
 	}
 	ctx.JSON(http.StatusOK, content)
@@ -65,54 +63,4 @@ func getRequestIP(c *gin.Context) string {
 		reqIP = "127.0.0.1"
 	}
 	return reqIP
-}
-func readLine(src string) []string {
-	fi, err := os.Open(src)
-	if err != nil {
-		log.Info.Printf("打开用户目录文件失败: %s\n", err)
-		return []string{}
-	}
-	defer func() {
-		if err := fi.Close(); err != nil {
-			log.Info.Printf("关闭用户目录文件失败: %s\n", err)
-		}
-	}()
-	names := []string{}
-	br := bufio.NewReader(fi)
-	for {
-		a, _, c := br.ReadLine()
-		if c == io.EOF {
-			break
-		}
-		names = append(names, string(a))
-		//log.Printf("读取到的用户(%s)\n", string(a))
-	}
-	return names
-}
-//ToDo 每次接收请求之前删除文件
-func removeAll(){
-	cmd := exec.Command("find", ".", "-name", "info.txt", "-exec", "rm",	"{}", "\\;")
-	//log.Info.Printf("生成的命令是:%s", cmd)
-	// 命令的错误输出和标准输出都连接到同一个管道
-	//stdout, err := cmd.StdoutPipe()
-	//cmd.Stderr = cmd.Stdout
-	//if err != nil {
-	//	log.Info.Printf("cmd.StdoutPipe产生的错误:%v", err)
-	//}
-	if err := cmd.Start(); err != nil {
-		log.Info.Printf("cmd.Run产生的错误:%v", err)
-	}
-	// 从管道中实时获取输出并打印到终端
-	//for {
-	//	tmp := make([]byte, 1024)
-	//	_, err := stdout.Read(tmp)
-	//	//写成输出日志
-	//	log.Info.Println(string(tmp))
-	//	if err != nil {
-	//		break
-	//	}
-	//}
-	if err := cmd.Wait(); err != nil {
-		log.Info.Println("命令执行中有错误产生", err)
-	}
 }
